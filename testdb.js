1662ca463520027ad19713012f8572a80f9a03be
sinjeeli <sinjeeli@gmail.com>
2023-07-26 20:56:43 -0400

    starter files

---
+++ testdb.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const config = require('./config/config.json');
+
+const sequelize = new Sequelize(config.development);
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+    }
+
+    testConnection();
+    
